daa144bd5ac1c2716691938171b61de0c48ef7c9
shailesh511 <shaileshtripathi511@gmail.com>
2022-12-22 00:13:33 +0530

    Some success in getting all users through api

--- frontend/src/Components/User.js
+++ frontend/src/Components/User.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import UserList from './UserList';
 export default function User() {
@@ -6,16 +6,20 @@ export default function User() {
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  let usersList = [];
+  const [usersList, setUsersList] = useState([]);
   const createUser = async (obj) => {
-    try{
-    const res = await axios.post("/createuser", obj);
-     console.log("response :"+ res);
+    try {
+      const res = await axios.post("/createuser", obj);
+      console.log("response :" + res);
     }
-    catch(err){
+    catch (err) {
       alert(err.response.data)
     }
   }
+
+  useEffect(() => {
+    console.log("userslist updated....");
+  }, [usersList])
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -27,13 +31,13 @@ export default function User() {
     createUser(obj);
   }
 
-  const getUsers = async()=>{
-    try{
+  const getUsers = async () => {
+    try {
       const res = await axios.get("/getusers");
-      usersList = res.data
+      setUsersList(res.data);
+      console.log(usersList);
     }
-    catch(e)
-    {
+    catch (e) {
       console.log(e);
     }
   }
@@ -49,9 +53,16 @@ export default function User() {
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
-      <div style={{"top":"250px","position":"absolute","left":"0px"}}>
-        <button onClick={getUsers}>get users</button>
-      <UserList/>
+      <button onClick={getUsers}>get users</button>
+      <div style={{ "top": "250px", "position": "absolute", "left": "0px" }}>
+
+        {
+          usersList.length > 0 ? usersList.map((ele) =>
+          // <UserList name={ele.name} email= {ele.email}/>
+           ele.name 
+           //ele.email 
+           ) : "No Data"
+      }
       </div>
     </div>
   )
--- frontend/src/Components/UserList.js
+++ frontend/src/Components/UserList.js
@@ -5,8 +5,8 @@ import React from 'react'
 export default function UserList(props) {
   return (
     <div>
-      <h2>name</h2>
-      <h4>email</h4>
+      <h2>{props.name}</h2>
+      <h4>{props.email}</h4>
     </div>
   )
 }
